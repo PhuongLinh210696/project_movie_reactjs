022bf601c704fce209a68f25243a8cb1e91a9d5b
Chí Công <chicongtkn@gmail.com>
2023-07-23 20:09:00 +0700

    add page Booking

---
+++ src/Pages/MovieBooking/MovieBooking.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Booking from '../../Components/Booking/Booking'
+
+const MovieBooking = () => {
+  return (
+    <div>
+        <Booking/>
+    </div>
+  )
+}
+
+export default MovieBooking
